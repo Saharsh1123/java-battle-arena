c11ab1e081ef7c828c979269080cbd7358df4d01
Saharsh1123 <saharshmarpadiga@gmail.com>
2026-07-23 13:09:07 -0400

    feat: add basic combat simulation

--- src/main/java/com/saharsh/battlearena/Main.java
+++ src/main/java/com/saharsh/battlearena/Main.java
@@ -1,10 +1,14 @@
 package com.saharsh.battlearena;
 
-/**
- * Hello world!
- */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Combatant fighter = new Combatant("fighter", 50, 300);
+        Combatant defender = new Combatant("defender", 30, 400);
+
+        System.out.println(defender.getHealth());
+
+        fighter.attack(defender);
+
+        System.out.println(defender.getHealth());
     }
 }
