c3905cd845509f7c0e2f61297b378b7a78e739dd
Saharsh1123 <saharshmarpadiga@gmail.com>
2026-07-25 20:57:39 -0400

    feat: add validated combatant name input

--- src/main/java/com/saharsh/battlearena/Main.java
+++ src/main/java/com/saharsh/battlearena/Main.java
@@ -1,13 +1,29 @@
 package com.saharsh.battlearena;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Combatant fighter = new Combatant("fighter", 50, 300);
+        Scanner scanner = new Scanner(System.in);
+        String name;
+
+        while (true) {
+            System.out.println("Enter Combatants name: ");
+            name = scanner.nextLine();
+
+            if (!name.isBlank()) {
+                break;
+            }
+
+            System.out.println("Name cannot be blank!");
+        }
+
+
+        Combatant fighter = new Combatant(name, 50, 300);
         Combatant defender = new Combatant("defender", 30, 400);
 
         while (defender.isAlive() && fighter.isAlive()) {
             System.out.println("Defender: %d".formatted(defender.getHealth()));
-            System.out.println("Fighter: %d".formatted(fighter.getHealth()));
+            System.out.println("%s: %d".formatted(name, fighter.getHealth()));
             fighter.attack(defender);
 
             if (defender.isAlive()) {
@@ -15,12 +31,14 @@ public class Main {
             }
         }
         System.out.println("Defender: %d".formatted(defender.getHealth()));
-        System.out.println("Fighter: %d".formatted(fighter.getHealth()));
+        System.out.println("%s: %d".formatted(name, fighter.getHealth()));
         
         if (fighter.getHealth() > 0) {
-            System.out.println("Winner: Fighter");
+            System.out.println("Winner: %s".formatted(name));
         } else {
             System.out.println("Winner: Defender");
         }
+
+        scanner.close();
     }
 }
