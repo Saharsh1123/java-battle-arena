75a5d9c0189617d57d191d558dae3548ff0857dd
Saharsh1123 <saharshmarpadiga@gmail.com>
2026-07-27 15:30:21 -0400

    feat: extract combatant creation and battle methods

--- src/main/java/com/saharsh/battlearena/Main.java
+++ src/main/java/com/saharsh/battlearena/Main.java
@@ -4,10 +4,42 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        Combatant fighter = createCombatant(scanner, "fighter");
+        Combatant defender = createCombatant(scanner, "defender");
+
+        runBattle(fighter, defender);
+
+        scanner.close();
+    }
+
+    static void runBattle(Combatant fighter, Combatant defender) {
+        while (defender.isAlive() && fighter.isAlive()) {
+            System.out.println("%s: %d".formatted(defender.getName(), defender.getHealth()));
+            System.out.println("%s: %d".formatted(fighter.getName(), fighter.getHealth()));
+            fighter.attack(defender);
+
+            if (defender.isAlive()) {
+                defender.attack(fighter);
+            }
+        }
+
+        System.out.println("%s: %d".formatted(defender.getName(), defender.getHealth()));
+        System.out.println("%s: %d".formatted(fighter.getName(), fighter.getHealth()));
+        
+        if (fighter.isAlive()) {
+            System.out.println("Winner: %s".formatted(fighter.getName()));
+        } else {
+            System.out.println("Winner: Defender");
+        }
+    }
+
+
+    static Combatant createCombatant(Scanner scanner, String role) {
         String name;
 
         while (true) {
-            System.out.println("Enter Combatants name: ");
+            System.out.println("Enter %ss name: ".formatted(role));
             name = scanner.nextLine();
 
             if (!name.isBlank()) {
@@ -17,30 +49,6 @@ public class Main {
             System.out.println("Name cannot be blank!");
         }
 
-
-        Combatant fighter = new Combatant(name, (int)(Math.random() * 41) + 20, (int)(Math.random() * 401) + 100);
-
-        Combatant defender = new Combatant("defender", (int)(Math.random() * 41) + 20, (int)(Math.random() * 401) + 100);
-
-
-        while (defender.isAlive() && fighter.isAlive()) {
-            System.out.println("Defender: %d".formatted(defender.getHealth()));
-            System.out.println("%s: %d".formatted(fighter.getName(), fighter.getHealth()));
-            fighter.attack(defender);
-
-            if (defender.isAlive()) {
-                defender.attack(fighter);
-            }
-        }
-        System.out.println("Defender: %d".formatted(defender.getHealth()));
-        System.out.println("%s: %d".formatted(fighter.getName(), fighter.getHealth()));
-        
-        if (fighter.isAlive()) {
-            System.out.println("Winner: %s".formatted(fighter.getName()));
-        } else {
-            System.out.println("Winner: Defender");
-        }
-
-        scanner.close();
+        return new Combatant(name, (int)(Math.random() * 41) + 20, (int)(Math.random() * 401) + 100);
     }
 }
